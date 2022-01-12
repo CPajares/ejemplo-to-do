65fab311ba1e0ca7437eba69d53c2fa52904bf41
Cpajares <c.pajares@hotmail>
2022-01-12 17:51:53 +0100

    Created function createTask at custom hook, conected with API

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,11 +1,25 @@
+import axios from "axios";
 import { useContext } from "react";
+import { createTaskAction } from "../store/actions/actionCreators";
 import tasksContext from "../store/contexts/tasksContext";
 
 const useTasks = () => {
   const { tasks, dispatch } = useContext(tasksContext);
 
+  const createTask = async (task) => {
+    const response = await axios.post(
+      "https://wk06todo.herokuapp.com/todo",
+      task
+    );
+
+    if (response.status === 201) {
+      dispatch(createTaskAction(response.data));
+    }
+  };
+
   return {
     tasks,
+    createTask,
   };
 };
 
