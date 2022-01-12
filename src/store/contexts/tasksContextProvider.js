190d7b1321b8d323c5120da04499f29877ff4875
Cpajares <c.pajares@hotmail>
2022-01-12 17:52:51 +0100

    Refactor Provider with capital letters and installed axios

--- src/store/contexts/tasksContextProvider.js
+++ src/store/contexts/tasksContextProvider.js
@@ -1,14 +1,14 @@
 import { useReducer } from "react";
 import tasksReducer from "../reducers/tasksReducer";
-import tasksContext from "./tasksContext";
+import TasksContext from "./tasksContext";
 
 const TasksContextProvider = ({ children }) => {
   const [tasks, dispatch] = useReducer(tasksReducer, []);
 
   return (
-    <tasksContext.Provider value={{ tasks, dispatch }}>
+    <TasksContext.Provider value={{ tasks, dispatch }}>
       {children}
-    </tasksContext.Provider>
+    </TasksContext.Provider>
   );
 };
 
