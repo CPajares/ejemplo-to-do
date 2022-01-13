152d5d6507a23cff7c2ac4711e9813fa562e409d
Cpajares <c.pajares@hotmail>
2022-01-13 15:52:41 +0100

    Included Edit function and conditional rendering to edit task

--- src/components/TaskCard/TaskCard.js
+++ src/components/TaskCard/TaskCard.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import useTasks from "../../hooks/useTasks";
 
 const TaskCard = ({ task }) => {
   const { modifyTask, deleteTask } = useTasks();
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [taskEdit, setTaskEdit] = useState(task);
+  const changeTask = (event) => {
+    setTaskEdit({ ...taskEdit, [event.target.id]: event.target.value });
+  };
+  const clickEdit = () => {
+    if (isEdit) {
+      modifyTask(taskEdit);
+    }
+    setIsEdit(!isEdit);
+  };
 
   const clickModify = () => {
     const newTask = { ...task, isDone: !task.isDone };
@@ -15,15 +28,48 @@ const TaskCard = ({ task }) => {
   return (
     <>
       <div>
-        <div>
-          <h3>{task.name}</h3>
-          <p>{task.description}</p>
-          <p>{task.date}</p>
-          <p>{task.isDone ? "Hecha" : "Pendiente"}</p>
-        </div>
+        {isEdit ? (
+          <div>
+            <input
+              value={taskEdit.name}
+              type="text"
+              id="name"
+              name="name"
+              onChange={changeTask}
+              placeholder="titulo"
+            />
+            <input
+              value={taskEdit.description}
+              type="text"
+              id="description"
+              name="description"
+              onChange={changeTask}
+              placeholder="descripcion"
+            />
+            <input
+              value={taskEdit.date}
+              type="date"
+              id="date"
+              name="date"
+              onChange={changeTask}
+              placeholder="fecha"
+            />
+
+            <p>{task.isDone ? "Hecha" : "Pendiente"}</p>
+          </div>
+        ) : (
+          <div>
+            <h3>{task.name}</h3>
+            <p>{task.description}</p>
+            <p>{task.date}</p>
+            <p>{task.isDone ? "Hecha" : "Pendiente"}</p>
+          </div>
+        )}
       </div>
       <div>
-        <button type="button">Editar tarea</button>
+        <button type="button" onClick={clickEdit}>
+          {isEdit ? "Guardar" : "Editar tarea"}
+        </button>
         <button type="button" onClick={clickDelete}>
           Eliminar tarea
         </button>
